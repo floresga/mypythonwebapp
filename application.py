fdf1152303bad1ed86b4fbf77b39c76d1dfb0705
guillermo.flores <guillermo.flores@mx.unisys.com>
2020-05-13 08:09:16 -0500

    Update application.py

--- application.py
+++ application.py
@@ -2,9 +2,5 @@ from flask import Flask, send_from_directory
 app = Flask(__name__)
 
 @app.route('/<path:path>')
-def send_files(path):
-    print path
-    # return send_from_directory('static', path)
-
-if __name__ == "__main__":
-    app.run()
+def hello(path):
+    return "[GUILLERMO] Hello World!"
